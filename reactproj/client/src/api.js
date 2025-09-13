28198dfa4fc032fb6e2b1e9547e824947e8d240d
greenpact <developer@greenpactconsulting.com>
2025-09-13 13:27:22 +0000

    fix2

--- reactproj/client/src/api.js
+++ reactproj/client/src/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend API base URL
+  //baseURL: "http://localhost:5000/api", / Your backend API base URL
+  baseURL: "https://gp-website.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
